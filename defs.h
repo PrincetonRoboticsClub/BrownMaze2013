856b4e7567be5fa1f14dd49cb799a7d54fdaaa7d
Ethan Gordon <ekgordon2@gmail.com>
2014-03-08 21:55:22 -0500

    Test Main

--- defs.h
+++ defs.h
@@ -10,10 +10,27 @@
 #define PIN_2B 4
 #define PIN_2N 5
 
+// Encoder Pin Numbers
 #define PIN_E1A 11
 #define PIN_E1B 12
 
 #define PIN_E2A 9
 #define PIN_E2B 10
+
+// Dimensions (cm)
+#define WHEEL_RADIUS 2
+#define WHEEL_BASE 7
+
+#define COUNT_PER_REV 1200
+
+// Other Constants
+#define TIMEOUT 10000
+
+// Included Libraries
+#include <Encoder.h>
+#include <Motor.h>
+#include <MotorSystem.h>
+#include <PositionTracker.h>
+#include <PID.h>
 
 #endif
---
+++ libraries/PID/PID.cpp
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+#include <PID.h>
+
+void PID::init(double p, double i, double d, double lo, double hi){
+  kp=p; ki=i; kd=d;
+  outMin=lo; outMax=hi;
+  reset();
+}
+
+
+PID::PID(double p,double i,double d,double lo,double hi){
+  init(p, i, d, lo, hi);
+}
+PID::PID(double p, double i, double d, double range){
+  init(p, i, d, -range, range);
+}
+PID::PID(double p,double i,double d){
+  init(p, i, d, -1.0, 1.0);
+}
+PID::PID(double k[3],double range[2]){
+  init(k[0], k[1], k[2], range[0], range[1]);
+}
+PID::PID(double k[3],double range){
+  init(k[0], k[1], k[2], -range, range);
+}
+PID::PID(double k[3]){
+  init(k[0], k[1], k[2], -1.0, 1.0);
+}
+  
+double PID::compute(double input, double setPoint){
+  double error = setPoint - input;
+  int temp = millis();
+  double dt=(double) (temp-lastT);    
+  
+  iTerm += (ki * error * dt/1000.0);
+  
+  if(iTerm > outMax){ iTerm= outMax; }
+  else { if(iTerm < outMin){ iTerm= outMin; } }
+  
+  double dInput = input - lastInput;
+
+  double output = kp*error + iTerm - kd*dInput;     
+  if(output > outMax){ output = outMax; }
+  else{ if(output < outMin){ output = outMin; } }
+  
+  lastInput = input;
+  lastT = temp;
+
+  return output;
+}
+double PID::compute(double input){
+  return compute(input, 0);
+}
+
+void PID::reset(){
+  iTerm=0;
+  lastT=millis();
+  lastInput=0;
+}
+void PID::setConstants(double p,double i,double d){
+  kp=p; ki=i; kd=d;
+}
